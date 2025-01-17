ae9ee48ddc66083dbb581acf57b6d207aa7ef72e
GETHOR2635 <162325255+GETHOR2635@users.noreply.github.com>
2025-01-17 22:37:57 +0545

    saved

---
+++ Assignment2.4/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Assignment2_4;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        var List = new List<Student>
+        {
+            new Student()
+            {
+                Name = "John",
+                Score = 86,
+            },
+            new Student()
+            {
+                Name = "Jane",
+                Score = 95,
+            },
+            new Student()
+            {
+                Name = "Jim",
+                Score = 58,
+            },
+            new Student()
+            {
+                Name = "Julie",
+                Score = 78,
+            },
+        };
+        
+        
+        Console.WriteLine("List of students:");
+        foreach (var item in List)
+        {
+            Console.WriteLine($"Name: {item.Name}, Score: {item.Score}");
+        }
+        Console.WriteLine("=====================");
+        
+        var highscore = List.OrderByDescending(x => x.Score).First();
+        Console.WriteLine($"Highscore: {highscore.Name}, Score: {highscore.Score}");
+        Console.WriteLine("===================");
+        var lowscore = List.OrderByDescending(x => x.Score).Last();
+        
+        Console.WriteLine($"Lowscore: {lowscore.Name}, Score: {lowscore.Score}");
+        
+            
+        
+    }
+}
+
+public class Student
+{
+    public string Name { get; set; }
+    public int Score { get; set; }
+}
